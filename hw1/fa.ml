5fe2ef71e1fe7c86e6b8831baacae776dbc670be
hotelcurtains <114700124+hotelcurtains@users.noreply.github.com>
2025-02-05 16:57:01 -0500

    is_deterministic

--- hw1/fa.ml
+++ hw1/fa.ml
@@ -33,6 +33,14 @@ let a2 = {states = ["q0";"q1";"q2";"q3";"q4"];
                ; ("q1",'c',"q2");  ("q3",'a',"q4")];
           final= ["q2"]
          }
+
+let a4 = {states = ["q0";"q1";"q2";"q3"];
+         start = "q0";
+         tf = [("q0",'a',"q1"); ("q1",'b',"q1")
+              ; ("q1",'c',"q2");  ("q0",'a',"q3")];
+         final= ["q2";"q3"]
+        }
+
 let tf_of_a = [("q0",'a',"q1"); ("q1",'b',"q1"); ("q1",'c',"q2")]
 
 
@@ -63,7 +71,7 @@ let rec apply_transition_function f st sym =
   | _::t -> apply_transition_function t st sym
 
 (* 
-Determine whether a word is accepted or recognized by a FA.
+Check whether a word is accepted or recognized by a FA.
 accept : fa -> input -> bool
 *)
 let rec accept fa word = 
@@ -77,3 +85,26 @@ let rec accept fa word =
       | None -> qo
   in List.mem (run fa.start word) fa.final
 
+(* 
+Returns the list of all the states that are successors of the given state
+with the given symbol.
+next : tf -> state -> symbol -> state list
+*)
+let rec next tf q l =
+  match tf with
+  | [] -> []
+  | (qi,s,qf)::t when qi = q && l = s -> qf::next t q l
+  | _::t -> next t q l
+
+(*
+Checks whether the given automaton is deterministic or not.
+is_deterministic : fa -> bool
+*)
+let rec is_deterministic fa =
+  let rec test q = 
+    match q with
+    | [] -> true
+    | (qi,s,qf)::t -> 
+      List.for_all (fun (x,y,z) -> not (x=qi && y=s && z<>qf)) t && test t
+  in
+  test fa.tf
